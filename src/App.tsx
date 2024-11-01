73439f5fd774bf380b799ae405004a451469b0e3
Krutik30 <agherakrutik99@gmail.com>
2024-11-01 22:15:00 +0530

    delete drive

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,6 @@ import {QueryClientProvider,QueryClient} from '@tanstack/react-query'
 import { router } from './router'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import useLocalStorage from './hooks/useLocalStorage';
-import { getUserHasLoggedIn } from './utils/getUserHasLoggedIn';
-import { useEffect, useState } from 'react';
-import { InitialCacheLoading } from './components/InitialCacheLoading';
 
 export type intialCacheLoadStatus = {
   agent: boolean, 
@@ -20,23 +16,12 @@ export type intialCacheLoadStatus = {
 const queryClient = new QueryClient();
 
 function App() {
-  const [userObject] = useLocalStorage('userObject', '');
-  const [hasLoggedIn, setHasLoggedIn] = useState(getUserHasLoggedIn());
-  const [initialCacheLoaded, setInitialCacheLoaded] = useState<intialCacheLoadStatus>({agent: false, customer: false, design: false, sizes: false, transport: false, });
 
-  useEffect(() => {
-    setHasLoggedIn(getUserHasLoggedIn());
-  }, [userObject]);
-  
-  console.log({hasLoggedIn, userObject, initialCacheLoaded});
   return (
     <ThemeProvider>
         <ToastContainer/>
         <QueryClientProvider client={queryClient}>
-          {(hasLoggedIn && userObject && Object.values(initialCacheLoaded).some(loadState => !loadState ))
-            ? <InitialCacheLoading initialCacheLoaded={initialCacheLoaded} setInitialCacheLoaded={setInitialCacheLoaded}/>
-            : <RouterProvider router={router} />
-          }
+          <RouterProvider router={router} />
         </QueryClientProvider>
     </ThemeProvider>
   )
--- src/db/localdb.ts
+++ src/db/localdb.ts
@@ -1,22 +1,10 @@
-import Dexie, { Table } from 'dexie';
-import { agentType, customerType, designType, transportType } from 'sb-schema-and-types';
+import Dexie from 'dexie';
 
-export const localdbStoresObj = {
-  customer: '++customer_name, updatedAt', // Primary key and indexed props
-  agent: '++agent_name, updatedAt',
-  transport: '++transport_name, updatedAt',
-  design: '++design_name, updatedAt'
-};
 export class LocalDbDexieClass extends Dexie {
-  // We just tell the typing system this is the case
-  customer!: Table<customerType>; 
-  agent!: Table<agentType>;
-  transport!: Table<transportType>; 
-  design!: Table<designType>;
 
   constructor() {
     super('smartbrand');
-    this.version(1).stores(localdbStoresObj);
+    this.version(1).stores({});
   }
 }
 
